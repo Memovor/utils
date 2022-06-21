c3d89ad7ed87de2e47374eff3aec0b5eef77f630
vsersionW <liangmr0505@163.com>
2022-06-21 16:14:40 +0800

    feat: 数组合并

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function objToTree() {
   
       // 定义格式化方法
       const fun = (arr, obj, val) => {
-        console.log(arr, obj, val, '输出')
         // 不能直接使用arr.length 因为这样就把最后一项也带出来了
         for(let i=0; i< arr.length-1; i++) {
           if (!obj[arr[i]]) obj[arr[i]] = {}
@@ -28,6 +27,22 @@ function objToTree() {
         // 调用格式化方法
         fun(key.split('-'), objTree, strarr[key])
       }
-      
-      console.log(objTree, 'objTree')
+      return objToTree
+}
+
+// 两数组合并为一个数组
+function mergeArr () {
+  // 1.concat方法 （concat方法不会改变原始数组，而是返回一个合并以后的新数组），需要进行多次的数组合并时会造成很大的内存浪费，适用于数据量比较小的场景
+  var arr1 = [1, 2, 3]
+  var arr2 = [4, 5, 6]
+  var arr3 = arr1.concat(arr2)
+  console.log(arr3) // arr3 = [1,2,3,4,5,6]
+
+  // 2.for循环，能避免内存浪费，缺点会让代码看起来冗余
+  for(var i in arr1) {
+    arr2.push(arr1[i]) // arr2 = [1,2,3,4,5,6]
+  }
+
+  // 3.apply(apply方法传递的参数列表是参数数组,会改变原数组)
+  arr1.push.apply(arr1, arr2) // arr1 = [1,2,3,4,5,6]
 }
